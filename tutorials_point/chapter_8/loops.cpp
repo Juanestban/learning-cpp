61764ee48b1dbd5687693cc24a5becdebf218d58
Juan Esteban <juanestban000@gmail.com>
2023-09-20 01:01:37 -0500

    feat(chapter_8): done loops

---
+++ tutorials_point/chapter_8/loops.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// while() {...}, for() {...}, do {...} while(), nested loops
+int main()
+{
+  /**
+   * break    => statement
+   * continue => statement
+   * goto     => statement
+   */
+
+  /**
+   * example infinite for loop
+    for ( ; ; ) {
+      printf("This loop will run forever\n");
+    }
+  */
+  return 0;
+}
