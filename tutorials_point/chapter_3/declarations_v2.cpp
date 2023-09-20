eb5080dc9ae96d38360acc9963e5ecba93adddf9
Juan Esteban <juanestban000@gmail.com>
2023-09-20 00:05:47 -0500

    feat(tutorials_point): chater 3 done

---
+++ tutorials_point/chapter_3/declarations_v2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// declaration function
+int func();
+int main()
+{
+  int result = func();
+  cout << "final_result: " << result << endl;
+
+  return 0;
+}
+
+int func()
+{
+  return 0;
+}
