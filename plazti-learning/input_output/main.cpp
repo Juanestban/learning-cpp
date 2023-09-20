379b2f304e77c93bd96b5847127d6c586d06132e
Juan Esteban <juanestban000@gmail.com>
2023-09-20 01:32:24 -0500

    feat(input_output): testing std cin

---
+++ plazti-learning/input_output/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int input;
+
+  cout << "put some number: ";
+  cin >> input;
+
+  cout << "number wrote: " << input << endl;
+
+  return 0;
+}
